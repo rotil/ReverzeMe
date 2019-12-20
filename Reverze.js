ed5ce08727c3a7e5bd0ba5a2d6902e1a117ac1e9
rotil <42150485+rotil@users.noreply.github.com>
2019-12-20 15:18:14 +0330

    Add files via upload

---
+++ Reverze.js
@@ -0,0 +1,19 @@
+Java.perform(function(){
+    console.log("\nreverzeme detection bypass with Frida");
+    var Attack = Java.use("com.example.reverzeme.ChallengeJNI");
+    console.log("\nHijacking com.example.reverzeme.ChallengeJNI function in Attack class");
+
+
+    Attack.checkIfDeviceIsEmulator.implementation = function(){
+        console.log("\nInside the checkIfDeviceIsEmulator function");
+         console.log("[*] Password - " + this.stringFromJNI());
+         console.log("[*] Flag - " + this.stringOtherHalfKey("s1r"));
+        return Java.use("java.lang.Boolean").$new("false");
+    }; 
+
+
+});
+
+
+//frida -Uf com.example.reverzeme -l reverzeme.js --no-pause
+
